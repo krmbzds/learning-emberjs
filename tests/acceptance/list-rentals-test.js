466ad22c43aa0a1ec9f3ff22d200d644fc82d74c
Kerem Bozdas <krmbzds.github@gmail.com>
2019-12-27 16:30:03 +0300

    Add failing tests

---
+++ tests/acceptance/list-rentals-test.js
@@ -0,0 +1,22 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'super-rentals/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | list rentals');
+
+test('should show rentals as the home page', function (assert) {
+});
+
+test('should link to information about the company', function (assert) {
+});
+
+test('should link to contact information', function (assert) {
+});
+
+test('should list available rentals', function (assert) {
+});
+
+test('should filter the list of rentals by city', function (assert) {
+});
+
+test('should show details for a selected rental', function (assert) {
+});
